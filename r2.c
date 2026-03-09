de1aff8383408acee45a5b7114b2cde33752d7dd
GOKUL-G078 <gokulg7789@gmail.com>
2026-03-09 11:07:29 +0530

    Refactor circle calculations to use radius

--- r2.c
+++ r2.c
@@ -8,15 +8,16 @@ float Perimeter(float);
 int main()
 {
     int x1, y1, x2, y2; 
-    float diameter, area, perimeter;
+    float diameter, area, perimeter,radius;
 
     printf("Enter coordinates of point 1 (x1 y1): ");
     scanf("%d %d", &x1, &y1);
     printf("Enter coordinates of point 2 (x2 y2): ");
     scanf("%d %d", &x2, &y2);
     diameter = Diameter((float)x1, (float)y1, (float)x2, (float)y2);
-    area = Area(diameter);
-    perimeter = Perimeter(diameter);
+    radius=diameter/2;
+    area = Area(radius);
+    perimeter = Perimeter(radius);
 
     printf("\n Circle Properties");
     printf("\nDiameter: %f", diameter);
@@ -30,13 +31,12 @@ float Diameter(float x1, float y1, float x2, float y2) 
     return sqrtf((x2 - x1)*2+ (y2 - y1)*2);
 }
 
-float Area(float diameter) 
+float Area(float radius) 
 {
-    float radius = diameter / 2.0f;
     return PI * (radius*2);
 }
 
-float Perimeter(float diameter) 
+float Perimeter(radius) 
 {
-    return PI * diameter;
+    return PI * 2*r;
 }
